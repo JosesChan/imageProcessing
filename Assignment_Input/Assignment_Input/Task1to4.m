fdb1c2f3d356da5369cc73f37ffd353c7995c32c
Joses <joses.chan@gmail.com>
2022-02-05 18:31:08 +0000

    Image processing evidence update

--- Assignment_Input/Assignment_Input/Task1to4.m
+++ Assignment_Input/Assignment_Input/Task1to4.m
@@ -23,10 +23,6 @@ scaleHeight = aimedHeight/height;
 % Used bicubic interpolation to retain aspect ratio
 resized_I_gray_unfiltered = imresize(resized_I_gray,[512, NaN],"bicubic");
 
-% Filter for noise
-h = fspecial("average",3);
-resized_I_gray= imfilter(resized_I_gray_unfiltered,h);
-
 width=size(resized_I_gray,1);
 height=size(resized_I_gray,2);
 newAspectRatio=height/width;
@@ -34,30 +30,35 @@ newAspectRatio=height/width;
 %figure, imshow(resized_I_gray)
 
 % Step-4: Produce histogram before enhancing, bin size 64
-%figure, imhist(resized_I_gray, 64)
+figure, imhist(resized_I_gray, 64)
 
 % Step-5: Enhance image before binarisation
+
+% Filter for noise
+h = fspecial("average",3);
+resized_I_gray= imfilter(resized_I_gray_unfiltered,h);
+
 imageIntensity = 255*im2double(resized_I_gray); % converts the intensity image I to double precision
 minimum = min(min(imageIntensity)); % find the minimum pixel intensity
 maximum = max(max(imageIntensity)); % find the maximum pixel intensity
 
 % Can use imadjust (Visual purposes) (Not detecting subtle features)
-% histeq (Less sensitive to noise) (Too much saturation)
-% adapthisteq (Enhances noise but highlights contrast for subtle features)
+%isteq (Less sensitive to noise) (Too much saturation)
+%adapthisteq (Enhances noise but highlights contrast for subtle features)
 
 A_enhanced_resized_I_gray = imadjust(resized_I_gray);
 B_enhanced_resized_I_gray = histeq(resized_I_gray);
 C_enhanced_resized_I_gray = adapthisteq(resized_I_gray);
 
-%figure, imshow(A_enhanced_resized_I_gray)
-%figure, imshow(B_enhanced_resized_I_gray)
-%figure, imshow(C_enhanced_resized_I_gray)
+figure, imshow(A_enhanced_resized_I_gray)
+figure, imshow(B_enhanced_resized_I_gray)
+figure, imshow(C_enhanced_resized_I_gray)
 
 % Step-6: Histogram after enhancement
 
-%figure, imhist(A_enhanced_resized_I_gray, 64)
-%figure, imhist(B_enhanced_resized_I_gray, 64)
-%figure, imhist(C_enhanced_resized_I_gray, 64)
+figure, imhist(A_enhanced_resized_I_gray, 64)
+figure, imhist(B_enhanced_resized_I_gray, 64)
+figure, imhist(C_enhanced_resized_I_gray, 64)
 
 
 % Step-7: Image Binarisation
@@ -65,7 +66,7 @@ A_binary_I = imbinarize(A_enhanced_resized_I_gray);
 B_binary_I = imbinarize(B_enhanced_resized_I_gray);
 C_binary_I = imbinarize(C_enhanced_resized_I_gray);
 
-figure, imshow(A_binary_I)
+%figure, imshow(A_binary_I)
 %figure, imshow(B_binary_I)
 %figure, imshow(C_binary_I)
 
@@ -77,13 +78,21 @@ edgeS = edge(chosen_I, "Sobel");
 edgeC = edge(chosen_I, "Canny");
 edgeL = edge(chosen_I, "log");
 
-%figure, imshow(edgeP)
-%figure, imshow(edgeS)
-%figure, imshow(edgeC)
-%figure, imshow(edgeL)
+figure, imshow(edgeP)
+figure, imshow(edgeS)
+figure, imshow(edgeC)
+figure, imshow(edgeL)
 
 % Task 3: Simple segmentation --------------------
 % Select image
+
+% Otsu thresholding
+level = graythresh(I_gray);
+
+BW = imbinarize(I_gray,level);
+
+figure, imshow(BW);
+
 segmentation_I = chosen_I;
 
 % Create images with padding of a white line
@@ -91,7 +100,6 @@ segmentation_I = chosen_I;
 nw_segmentation_I=padarray(segmentation_I,[1 1],1,"pre"); ne_segmentation_I=padarray(segmentation_I,[1 0],1,"pre");
 ne_segmentation_I=padarray(ne_segmentation_I,[0 1],1,"post"); se_segmentation_I=padarray(segmentation_I,[1 1],1,"post");
 sw_segmentation_I=padarray(segmentation_I,[1 0],1,"post"); sw_segmentation_I=padarray(sw_segmentation_I,[0 1],1,"pre");
-
 
 % Image fill on all border pad image to gain border image segments
 nw_segmentation_I=imfill(nw_segmentation_I,"holes");ne_segmentation_I=imfill(ne_segmentation_I,"holes");
@@ -108,24 +116,31 @@ segmentation_I=nw_segmentation_I|ne_segmentation_I|se_segmentation_I|sw_segmenta
 % Remove small objects with less than 100 pixels
 segmentation_I=bwareaopen(segmentation_I,100);
 
-figure, imshow(segmentation_I)
-
+figure, imshow(segmentation_I);
 
 % Task 4: Object Recognition --------------------
 
 % Find bloodcells within the image, by looking at 
 [centers, radii] = imfindcircles(segmentation_I, [50 1000], "Sensitivity", 0.95, "method", "TwoStage");
-%h = viscircles(centers, radii,'Color','r');
-%maskBloodCell = [centers, radii];
 
 % Create mask
-[x y]=meshgrid(1:size(I_gray,2),1:size(I_gray,1));
+[x, y]=meshgrid(1:size(I_gray,2),1:size(I_gray,1));
 mask=zeros(size(I_gray));
 
+% Create a mask out of each circle found
 for i=1:numel(radii)
     mask = mask | (x-centers(i,1)).^2+(y-centers(i,2)).^2<=radii(i).^2;
 end
 
-bloodcell = segment_I.*mask;
-figure, imshow(segment_I.*mask);
+[B,L] = bwboundaries(mask,'noholes');
+imshow(label2rgb(L, @jet, [0 0 0]))
+hold on
+for k = 1:length(B)
+    boundary = B{k};
+    plot(boundary(:,2), boundary(:,1), 'r', 'LineWidth', 2)
+end
+
+
+imshow(labeloverlay(I_gray,mask,'Transparency',0))
+
 
--- Assignment_Input/Assignment_Input/Task5to6.m
+++ Assignment_Input/Assignment_Input/Task5to6.m
@@ -2,7 +2,7 @@
 
 % Task 1: Pre-processing -----------------------
 % Step-1: Load input image
-I = imread('IMG_11.png');
+I = imread('IMG_13.png');
 
 % Step-2: Covert image to grayscale
 I_gray = rgb2gray(I);
@@ -15,13 +15,6 @@ I_gray = imsharpen(I_gray);
 I_gray = imopen(I_gray, SE);
 figure, imshow(I_gray);
 
-% Step 3: Morpohology
-%se = strel('disk',4);
-%morph_gray = imclose(I_gray, se);
-%morph_gray = imsharpen(morph_gray);
-%morph_gray = imfill(morph_gray);
-%figure, imshow(morph_gray);
-
 % Contour Method
 % Create mask from image size, difference of 1 row and 1 col
 mask = zeros(size(I_gray));
@@ -30,8 +23,10 @@ figure, imshow(mask);
 
 % Apply active contour alg
 segment_I = activecontour(I_gray,mask,500);
+figure, imshow(segment_I);
 % Remove small blobs
 segment_I = bwareaopen(segment_I, 150);
+figure, imshow(segment_I);
 
 % Find bloodcells within the image, by looking at 
 [centers, radii] = imfindcircles(segment_I, [50 1000], "Sensitivity", 0.95, "method", "TwoStage");
@@ -40,6 +35,7 @@ segment_I = bwareaopen(segment_I, 150);
 [x, y]=meshgrid(1:size(I_gray,2),1:size(I_gray,1));
 mask=zeros(size(I_gray));
 
+% Create a mask out of each circle found
 for i=1:numel(radii)
     mask = mask | (x-centers(i,1)).^2+(y-centers(i,2)).^2<=radii(i).^2;
 end
@@ -47,10 +43,8 @@ end
 bloodcell = segment_I.*mask;
 figure, imshow(segment_I.*mask);
 
-% Task 6: Performance evaluation -----------------
-% Step 1: Load ground truth data
-GT = imread("IMG_11_GT.png");
 
+% Creation of masks to identify blood, bacteria and background
 bloodcell = imbinarize(bloodcell);
 bacteria = xor(segment_I, bloodcell);
 background = zeros(size(I_gray));
@@ -59,11 +53,18 @@ figure, imshow(bloodcell);
 figure, imshow(bacteria);
 figure, imshow(background);
 
-similarity = dice(bloodcell, imbinarize(GT));
 
-figure
-imshowpair(bloodcell, imbinarize(GT));
-title(['Dice Index = ' num2str(similarity)]);
+% Task 6: Performance evaluation -----------------
+% Step 1: Load ground truth data
+%GT = imread("IMG_11_GT.png");
+%L_GT = label2rgb(GT, 'prism','k','shuffle');
+
+% Determine similarity between bloodcell and groundtruth image
+%similarity = dice(bloodcell, imbinarize(GT));
+
+%figure
+%imshowpair(bloodcell, imbinarize(GT));
+%title(['Dice Index = ' num2str(similarity)]);
 
 %[precision] = bfscore(bloodcell,GT(1));
 %[recall] = bfscore(bloodcell,GT(1));
